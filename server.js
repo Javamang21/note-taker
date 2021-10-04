395812ce45c6f292732a04e8c35fe06c024f79db
Javamang21 <juliustovar96@gmail.com>
2021-10-04 13:39:05 -0400

    Added nodemon for live server changes and linked static html file to a get route

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
 const express = require('express');
+const nodemon = require('nodemon');
 
-
+const PORT = process.env.PORT || 3006;
 const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Root directory for routing, 
+app.use(express.static('Develop/public'));
+
+app.get('/index.html', (req, res) => {
+    res.send('index');
+});
+
+app.get('/notes.html', (req, res) => {
+    res.send('notes');
+});
+
+// Not Found response for unmatched routes
+app.use((req, res) => {
+    res.status(404).end();
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
